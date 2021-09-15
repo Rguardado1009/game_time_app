8f3200795480ea4ee4f23a3af3f3719a3a1edc00
Rguardado1009 <rguardado1009@gmail.com>
2021-09-15 02:17:54 -0400

    updates

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './App.css';
-import homeLogo from "/Users/ronald/Development/project/game_time_app/game-time-app-frontend/src/Images/1x/Artboard 57.png" 
 
 export default function Home() {
     return (
         <div className="main-container">
             <h1 className= "App-title"> Welcome to Game Time! </h1>
-            <br/>
-            <button className = "homeBtn"> Create My Team</button>
+            <button className = "homeBtn"> View Leagues</button>
         </div>
     )
 }
